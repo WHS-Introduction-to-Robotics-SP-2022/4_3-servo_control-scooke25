2e12b64200fcfa242142e534bfe30e862e903463
scooke25 <99276515+scooke25@users.noreply.github.com>
2022-04-19 10:01:53 -0600

    Create 4.3_Servo.ino

---
+++ 4.3_Servo.ino
@@ -0,0 +1,29 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 100;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(pos);
+  delay(1000);
+  myservo.write(20);
+  delay(1000);
+  myservo.write(50); 
+  
+}
+
+void loop() {
+ 
+}
